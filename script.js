b58d8352c5d40a998a0864fd5e3303e51efc6d07
Axel <axelarep@gmail.com>
2022-03-15 01:00:29 -0600

    The structure of page is fully complete, it works. The next step is to polish the overall style

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//User Selection Image
+let userSelection = document.querySelector('#userSelection');
+
+//CPU Selection Image
+let cpuSelection = document.querySelector('#cpuSelection');
+
 //Button event listeners
 let btnRock = document.querySelector('button#btnRock')
 let rockClicked = btnRock.addEventListener('click', function(){  //Click event to 'Rock' button
@@ -39,58 +45,122 @@ function playGame(val){  //Game function. It will be renamed and optimized
 
     //Logic of Rock
     if (val == 'rock' && randomItem == 'rock') {
-        resultDisplay.innerText = 'EMPATES ' + 'User = Rock. ' 
-        + userPoints + ' Puntos. ' + 'CPU ' + cpuPoints + ' Puntos';
+
+        resultDisplay.innerHTML='<h1>Tie</h1>'
+
+        userSelection.innerHTML='<img src="images/rock.jpeg" alt=""><h3>You = ' + userPoints + ' Points</h3>'
+
+        cpuSelection.innerHTML='<img src="images/rock.jpeg" alt=""><h3>CPU = ' + cpuPoints + ' Points</h3>'
+
         randomItem = items[Math.floor(Math.random()*items.length)];
 
     }else if(val == 'rock' && randomItem == 'paper'){
-        resultDisplay.innerText = 'PIERDES' + 'User = Paper. ' 
-        + userPoints + ' Puntos. ' + 'CPU ' + (cpuPoints+=1) + ' Puntos';
+
+        resultDisplay.innerHTML='<h1>You Lose</h1>'
+
+        userSelection.innerHTML='<img src="images/rock.jpeg" alt=""><h3>You = ' + userPoints + ' Points</h3>'
+
+        cpuSelection.innerHTML='<img src="images/paper.jpeg" alt=""><h3>CPU = ' + (cpuPoints+=1) + ' Points</h3>'
+
+        // resultDisplay.innerText = 'PIERDES' + 'User = Paper. ' 
+        // + userPoints + ' Puntos. ' + 'CPU ' + (cpuPoints+=1) + ' Puntos';
         randomItem = items[Math.floor(Math.random()*items.length)];
 
     }else if(val == 'rock' && randomItem == 'scissors'){
-        resultDisplay.innerText = 'GANAS' + 'User = Scissors. ' 
-        + (userPoints+=1) + ' Puntos. ' + 'CPU ' + cpuPoints + ' Puntos';
+
+        resultDisplay.innerHTML='<h1>You Win</h1>'
+
+        userSelection.innerHTML='<img src="images/rock.jpeg" alt=""><h3>You = ' + (userPoints+=1) + ' Points</h3>'
+
+        cpuSelection.innerHTML='<img src="images/scissors.jpeg" alt=""><h3>CPU = ' + cpuPoints + ' Points</h3>'
+
+        // resultDisplay.innerText = 'GANAS' + 'User = Scissors. ' 
+        // + (userPoints+=1) + ' Puntos. ' + 'CPU ' + cpuPoints + ' Puntos';
         randomItem = items[Math.floor(Math.random()*items.length)];
 
     //Logic of Paper
     }else if(val == 'paper' && randomItem == 'rock') {
-        resultDisplay.innerText = 'GANAS ' + 'User = Rock. ' 
-        + (userPoints+=1) + ' Puntos. ' + 'CPU ' + cpuPoints + ' Puntos';
+
+        resultDisplay.innerHTML='<h1>You Win</h1>'
+
+        userSelection.innerHTML='<img src="images/paper.jpeg" alt=""><h3>You = ' + (userPoints+=1) + ' Points</h3>'
+
+        cpuSelection.innerHTML='<img src="images/rock.jpeg" alt=""><h3>CPU = ' + cpuPoints + ' Points</h3>'
+
+        // resultDisplay.innerText = 'GANAS ' + 'User = Rock. ' + (userPoints+=1) + ' Puntos. ' + 'CPU ' + cpuPoints + ' Puntos';
         randomItem = items[Math.floor(Math.random()*items.length)];
 
     }else if(val == 'paper' && randomItem == 'paper'){
-        resultDisplay.innerText = 'EMPATES' + 'User = Paper. ' 
-        + userPoints + ' Puntos. ' + 'CPU ' + cpuPoints + ' Puntos';
+
+        resultDisplay.innerHTML='<h1>Tie</h1>'
+
+        userSelection.innerHTML='<img src="images/paper.jpeg" alt=""><h3>You = ' + userPoints + ' Points</h3>'
+
+        cpuSelection.innerHTML='<img src="images/paper.jpeg" alt=""><h3>CPU = ' + cpuPoints + ' Points</h3>'
+
+        // resultDisplay.innerText = 'EMPATES' + 'User = Paper. ' 
+        // + userPoints + ' Puntos. ' + 'CPU ' + cpuPoints + ' Puntos';
         randomItem = items[Math.floor(Math.random()*items.length)];
 
     }else if(val == 'paper' && randomItem == 'scissors'){
-        resultDisplay.innerText = 'PIERDES' + 'User = Scissors. ' 
-        + userPoints + ' Puntos. ' + 'CPU ' + (cpuPoints+=1) + ' Puntos';
+
+        resultDisplay.innerHTML='<h1>You Lose</h1>'
+
+        userSelection.innerHTML='<img src="images/paper.jpeg" alt=""><h3>You = ' + userPoints + ' Points</h3>'
+
+        cpuSelection.innerHTML='<img src="images/scissors.jpeg" alt=""><h3>CPU = ' + (cpuPoints+=1) + ' Points</h3>'
+
+        // resultDisplay.innerText = 'PIERDES' + 'User = Scissors. ' 
+        // + userPoints + ' Puntos. ' + 'CPU ' + (cpuPoints+=1) + ' Puntos';
         randomItem = items[Math.floor(Math.random()*items.length)];
 
     //Logic of Scissors
     }else if(val == 'scissors' && randomItem == 'rock') {
-        resultDisplay.innerText = 'PIERDES' + 'User = Rock. ' 
-        + userPoints + ' Puntos. ' + 'CPU ' + (cpuPoints+=1) + ' Puntos';
+
+        resultDisplay.innerHTML='<h1>You Lose</h1>'
+
+        userSelection.innerHTML='<img src="images/scissors.jpeg" alt=""><h3>You = ' + userPoints + ' Points</h3>'
+
+        cpuSelection.innerHTML='<img src="images/rock.jpeg" alt=""><h3>CPU = ' + (cpuPoints+=1) + ' Points</h3>'
+
+        // resultDisplay.innerText = 'PIERDES' + 'User = Rock. ' 
+        // + userPoints + ' Puntos. ' + 'CPU ' + (cpuPoints+=1) + ' Puntos';
         randomItem = items[Math.floor(Math.random()*items.length)];
 
     }else if(val == 'scissors' && randomItem == 'paper'){
-        resultDisplay.innerText = 'GANAS' + 'User = Paper. ' 
-        + (userPoints+=1) + ' Puntos. ' + 'CPU ' + cpuPoints + ' Puntos';
+
+        resultDisplay.innerHTML='<h1>You Win</h1>'
+
+        userSelection.innerHTML='<img src="images/scissors.jpeg" alt=""><h3>You = ' + (userPoints+=1) + ' Points</h3>'
+
+        cpuSelection.innerHTML='<img src="images/paper.jpeg" alt=""><h3>CPU = ' + cpuPoints + ' Points</h3>'
+
+        // resultDisplay.innerText = 'GANAS' + 'User = Paper. ' 
+        // + (userPoints+=1) + ' Puntos. ' + 'CPU ' + cpuPoints + ' Puntos';
         randomItem = items[Math.floor(Math.random()*items.length)];
 
     }else if(val == 'scissors' && randomItem == 'scissors'){
-        resultDisplay.innerText = 'EMPATES' + 'User = Scissors. ' 
-        + userPoints + ' Puntos. ' + 'CPU ' + cpuPoints + ' Puntos';
+
+        resultDisplay.innerHTML='<h1>Tie</h1>'
+
+        userSelection.innerHTML='<img src="images/scissors.jpeg" alt=""><h3>You = ' + userPoints + ' Points</h3>'
+
+        cpuSelection.innerHTML='<img src="images/scissors.jpeg" alt=""><h3>CPU = ' + cpuPoints + ' Points</h3>'
+
+        // resultDisplay.innerText = 'EMPATES' + 'User = Scissors. ' 
+        // + userPoints + ' Puntos. ' + 'CPU ' + cpuPoints + ' Puntos';
         randomItem = items[Math.floor(Math.random()*items.length)];
 }
 
 //This If Else statement displays a win or lose message to the first one (between User or CPU) who reaches 5 points.
 if (userPoints == 5) {
-    resultDisplay.innerText = 'YOU WIN!'
+    resultDisplay.style.color='white';
+    resultDisplay.style.textAlign='center';
+    resultDisplay.innerText = 'YOU WIN!';
     }else if(cpuPoints == 5){
-    resultDisplay.innerText = 'YOU LOSE'
+    resultDisplay.style.color='white';
+    resultDisplay.style.textAlign='center';
+    resultDisplay.innerText = 'YOU LOSE';
     }
 
 };
